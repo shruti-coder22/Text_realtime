65225a7ad9dff9b1af53aae6ce90b181c1cae4cf
shruti-coder22 <79634442+shruti-coder22@users.noreply.github.com>
2021-10-10 12:33:18 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -18,36 +18,3 @@ function setup() {
 function modelLoaded() {
     console.log("poseNet is initialized!");
 }
-
-function draw() {
-    background("#20B2AA");
-    let div = createDiv('Shruti');
-    div.style('font-size', difference);
-    div.position(nosex, nosey);
-
-
-    document.getElementById("text_size").innerHTML = "The text's size is " + difference + " px."
-}
-
-function gotPoses(results) {
-    if (results.length > 0) {
-        console.log(results);
-        
-        nosex = results[0].pose.nose.x;
-        nosey = results[0].pose.nose.y;
-
-        leftWrist = results[0].pose.leftWrist.x;
-        rightWrist = results[0].pose.rightWrist.y;
-        difference = floor(rightWrist - leftWrist);
-
-        if(difference < 0) {
-            difference = difference * -1;
-        }
-
-        console.log("Nose X = " + nosex);
-        console.log("Nose Y = " + nosey);
-        console.log("Left Wrist = " + leftWrist);
-        console.log("Right Wrist = " + rightWrist);
-        console.log("Difference = " + difference);
-    }
-}
